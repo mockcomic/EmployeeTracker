5ba94eb5bed10667f6e8016455b160ccadfab246
Kurtis <gears2525@aol.com>
2021-12-08 22:22:58 -0800

    backing up for laptop

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-DROP DATABASE IF EXISTS inventory_db;
-CREATE DATABASE inventory_db;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
 
-USE inventory_db;
+USE employees;
 
 CREATE TABLE department ( 
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
+  department_id INTEGER NOT NULL,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manger_id INT NOT NULL, 
-  FOREIGN KEY (id) REFERENCES employee(manger_id),
-  FOREIGN KEY (role_id) REFERENCES role(id)
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL, 
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,25 @@
 INSERT INTO department(name)
-VALUES("The Great Gatsby");
+VALUES("Service","Sales","Engineering","Finance","Legal");
 
 
 INSERT INTO role(title,salary,department_id)
-VALUES('test','test','test');
+VALUES
+('Salesperson','80000',2),
+('Lead Engineer','150000',3),
 
 
-INSERT INTO employee(first_name,last_name,role_id,manger_id)
-VALUES('test','test','test','test');
+
+INSERT INTO employee(first_name,last_name)
+VALUES
+  ('James', 'Fraser'),
+  ('Jack', 'London'),
+  ('Robert', 'Bruce'),
+  ('Peter', 'Greenaway'),
+  ('Derek', 'Jarman'),
+  ('Paolo', 'Pasolini'),
+  ('Heathcote', 'Williams'),
+  ('Sandy', 'Powell'),
+  ('Emil', 'Zola'),
+  ('Sissy', 'Coalpits'),
+  ('Antoinette', 'Capet'),
+  ('Samuel', 'Delany');
